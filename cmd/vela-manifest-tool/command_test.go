9ed04751eccb62859508188b398edf02f4c26617
Kelly Merrick <kelly.merrick@target.com>
2025-08-01 17:31:02 -0500

    fix(linter): pass the context to appease linter

--- cmd/vela-manifest-tool/command_test.go
+++ cmd/vela-manifest-tool/command_test.go
@@ -44,7 +44,7 @@ func TestExecution(t *testing.T) {
 		var outbuf, errbuf bytes.Buffer
 
 		stdout, stderr = &outbuf, &errbuf
-		cmd := exec.Command(tc.args[0], tc.args[1:]...) //nolint:gosec // we control the test data
+		cmd := exec.CommandContext(t.Context(), tc.args[0], tc.args[1:]...) //nolint:gosec // we control the test data
 
 		err := execCmd(cmd)
 		if err != nil {
